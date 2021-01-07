151e0f26efaeb46bda6719de71fec9f3d3278bba
PSTkachev <P.S.Tkachev@gmail.com>
2021-01-07 20:39:52 +0300

    Проверка элемента на странице + assert

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,26 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
+
         """конструктор — метод, который вызывается, когда мы создаем объект. 
         Конструктор объявляется ключевым словом __init__. 
         В него в качестве параметров мы передаем экземпляр драйвера и url адрес. 
         Внутри конструктора сохраняем эти данные как аттрибуты нашего класса."""
+
     def open(self):
         self.browser.get(self.url)
         """метод открывает нужную страницу, используя метод get()"""
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
+
+    """метод, в котором будем перехватывать исключение. В него будем передавать два аргумента: 
+    как искать(css, id, xpath и тд) и собственно что искать(строку - селектор)."""
